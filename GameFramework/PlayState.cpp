7e50d0a98a2a9bc75d11054f61bd7ed961913a5d
창훈 박 <wawworld@imrlab.hoseo.edu>
2019-09-08 22:06:32 +0900

    GF17.PlayState

--- GameFramework/PlayState.cpp
+++ GameFramework/PlayState.cpp
@@ -1,24 +1,50 @@
-#include "PlayState.h"
+#include "Game.h"
 #include <iostream>
 
 const std::string PlayState::s_playID = "PLAY";
 void PlayState::update()
 {
-	// nothing for now
+	for (int i = 0; i < m_gameObjects.size(); i++)
+	{
+		m_gameObjects[i]->update();
+	}
+
 }
 void PlayState::render()
 {
-	// nothing for now
+	for (int i = 0; i < m_gameObjects.size(); i++)
+	{
+		m_gameObjects[i]->draw();
+	}
+
 }
 
 bool PlayState::onEnter()
 {
 	std::cout << "entering PlayState\n";
+	if (!TheTextureManager::Instance()->load(
+		"assets/helicopter.png", "helicopter",
+		TheGame::Instance()->getRenderer()))
+	{
+		return false;
+	}
+
+
+	GameObject* player = new Player(
+		new LoaderParams(100, 100, 128, 55, "helicopter"));
+	m_gameObjects.push_back(player);
 	return true;
 }
 
 bool PlayState::onExit()
 {
 	std::cout << "exiting PlayState\n";
+	for (int i = 0; i < m_gameObjects.size(); i++)
+	{
+		m_gameObjects[i]->clean();
+	}
+	m_gameObjects.clear();
+
+	TheTextureManager::Instance()->clearFromTextureMap("helicopter");
 	return true;
 }
--- GameFramework/Player.cpp
+++ GameFramework/Player.cpp
@@ -4,38 +4,41 @@
 Player::Player(const LoaderParams* pParams) : SDLGameObject(pParams)
 {
 }
+
 void Player::draw()
 {
-	SDLGameObject::draw(); // we now use SDLGameObject
+	SDLGameObject::draw(); 
+	if (m_velocity.getX() > 0)
+	{
+		TextureManager::Instance()->drawFrame(m_textureID,
+			(Uint32)m_position.getX(), (Uint32)m_position.getY(),
+			m_width, m_height, m_currentRow, m_currentFrame,
+			TheGame::Instance()->getRenderer(),
+			SDL_FLIP_HORIZONTAL);
+	}
+	else {
+		TextureManager::Instance()->drawFrame(m_textureID,
+			(Uint32)m_position.getX(), (Uint32)m_position.getY(),
+			m_width, m_height, m_currentRow, m_currentFrame,
+			TheGame::Instance()->getRenderer());
+	}
 }
 
 void Player::update()
 {
 	m_velocity.setX(0);
 	m_velocity.setY(0);
-	handleInput(); // add our function
+	handleInput(); 
 	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
 	SDLGameObject::update();
 }
 
 void Player::handleInput()
 {
-	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_RIGHT))
-	{
-		m_velocity.setX(2);
-	}
-	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_LEFT))
-	{
-		m_velocity.setX(-2);
-	} 
-	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_UP))
-	{
-		m_velocity.setY(-2);
-	}
-	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_DOWN))
-	{
-		m_velocity.setY(2);
-	}
+	Vector2D* target = TheInputHandler::Instance()->getMousePosition();
+	m_velocity = *target - m_position;
+	m_velocity /= 50;
+
 	if (TheInputHandler::Instance()->isKeyOneDown(SDL_SCANCODE_SPACE)==1)
 	{
 		TheGame::Instance()->pushBack(new Enemy(new LoaderParams(
